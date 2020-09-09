978d9674e1b6f5bdfc062736df6967fd2ee5f795
Raffael Andre <42149834+r-andre@users.noreply.github.com>
2020-09-09 16:34:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,10 +24,45 @@ def log(message:str):
     file.write(str(pd.Timestamp.today()) + " " + message + "\n")
     file.close()
 
-def scrape(url:str, main:bool):
+def robot(url:str) -> list:
+    '''Stores all sites and content disallowed by the robot.txt in a list.'''
+    r = url + "/robots.txt"
+    disallow = [] # list storing urls of all disallowed content of a site
+
+    try:
+        # Establishing the connection to the robots.txt
+        response = requests.get(r, timeout=3)
+    except:
+        # If the robots.txt is not accessible, the whole website is added to
+        # the disallowed list:
+        log("Error! Response timeout for " + r)
+        disallow.append(re.search("[^/]*//[^/]*", url).group())
+    else:
+        soup = BeautifulSoup(response.text, "lxml")
+        text = soup.get_text()
+        # Rules for all user agents * will be added to the disallowed list:
+        for txt in text.replace("\n", "linebreak").split("User-"):
+            if re.search("agent: \*", txt):
+                rules = txt.split("linebreak")
+                for rule in rules:
+                    if "Disallow" in rule:
+                        disallow.append(rule.replace("Disallow: ", ""))
+                break
+            else:
+                pass
+
+    return disallow
+
+def scrape(url:str, main:bool, disallow:list=[]):
     '''Takes all text content of a website for an input URL and stores it in a
     global variable. May also store all links to sub-sites on the main page in
     a separate list in order to scrape them later.'''
+    # Checking if there is a list of disallowed content for the site:
+    if main and len(disallow) > 0:
+        # If the entire site is disallowed, stop scraping:
+        if "/" in disallow:
+            log("Disallowed " + url)
+            return
     try:
         # Establishing the connection:
         response = requests.get(url, timeout=3)
@@ -48,18 +83,20 @@ def scrape(url:str, main:bool):
                 # Storing the text of the site:
                 text = clean(soup.get_text(" ", strip=True))
             except:
-                # If no text could be scraped, return an error message:
-                log("Error! No text could be scraped for " + url) # log message
+                # If no text could be scraped, return a warning message:
+                log("Warning! No text could be scraped for " + url) # log message
             else:
                 # Append the scraped information to the list of contents:
                 timestamp = str(pd.Timestamp.today().date())
-                contents.append((re.search("[^/]*//[^/]*/", url).group(), url, title, text.replace(title, ""), timestamp))
+                contents.append((re.search("[^/]*//[^/]*", url).group(), url, title, text.replace(title, ""), timestamp))
 
             if main:
                 # If the scraped site is the main site the also collect all available links to sub-sites:
                 for a in soup.findAll('a', href=True):
                     href = a['href']
-                    if href.startswith("/") and len(href) > 1:
+                    # Checking if the href is not part of the disallow list, that it is not a reference back to the
+                    # main site ("/"), not an external link and not a special format (.jpeg, etc.):
+                    if href.startswith("/") and len(href) > 1 and "." not in href and href not in disallow:
                         hrefs.append(re.search("[^/]*//[^/]*", url).group() + href)
                         # Note: re.search is used to sidestep problems of some multi-language sites e.g. that
                         # are accessed with a /de/ version of their site, causing href links to then refer to
@@ -68,6 +105,21 @@ def scrape(url:str, main:bool):
         else:
             # If access to the site was denied, return an error message:
             log("Error! Response code " + str(response.status_code) + " for " + url) # log message
+
+def validate(df:pd.DataFrame):
+    '''Checks if the scraped data is valid and as expected.'''
+    log("Running validity check")
+    if df.empty:
+        # End the program if no data was scraped
+        log("Validity check failure: no data, program ended after " + str(pd.Timestamp.now() - start))
+        raise Exception("Validity check failure: empty dataframe")
+    else:
+        if df.isnull().values.any():
+            # Check for empty cells in the table
+            log("Validity check: empty detected")
+        if df.duplicated().any():
+            # Check for duplicate lines in the table
+            log("Validity check: duplicates detected")
 
 def db(df:pd.DataFrame, table:str='Test'):
     '''Connects to or creates and SQLite database and stores all scraped
@@ -79,28 +131,13 @@ def db(df:pd.DataFrame, table:str='Test'):
         connection = sqlite3.connect(base)
         # Writing the data to the specified table:
         df.to_sql(table, connection, index=False, if_exists='append')
-        log("Connection established to " + base + ", storing scraped data in table " + table)
+        log("Connection established to \"" + base + "\", storing scraped data in table \"" + table + "\"")
     except sqlite3.Error as error:
         # If no connection could be established, print the Error message and save the date in a csv file:
         log("Connection to " + base + " failed: " + str(error))
         filename = 'backup.csv'
         log("Saving scraped data as " + filename)
         df.to_csv(filename, index=False, sep="\t")
-
-def validate(df:pd.DataFrame):
-    '''Checks if the scraped data is valid and as expected.'''
-    if df.empty:
-        # End the program if no data was scraped
-        log("No data was scraped, terminating program")
-        raise Exception("Validity check failure: empty dataframe")
-    else:
-        log("Running validity check")
-    if df.isnull().values.any():
-        # Check for empty cells in the table
-        log("Validity check: empty detected")
-    if df.duplicated().any():
-        # Check for duplicate lines in the table
-        log("Validity check: duplicates detected")
 
 if __name__ == "__main__":
     start = pd.Timestamp.now()
@@ -122,7 +159,7 @@ if __name__ == "__main__":
 
     # Scraping every site in the list of websites:
     for site in websites:
-        scrape(site, main=True)
+        scrape(site, main=True, disallow=robot(site))
 
     # Shuffling the list of sub-sites to avoid sending too many requests to the same site one after another:
     hrefs = list(set(hrefs))
